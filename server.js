be93a2dd26c625e91df2a18b995bbe6e71c1b43a
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-10-27 22:16:48 -0700

    Set up basic server middleware on server.js

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 const express = require("express");
+// middleware set up
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
 const server = express();
+
+/*
+morgan("dev"):
+Concise output colored by response status for development use. 
+The :status token will be colored red for server error codes, 
+yellow for client error codes, 
+cyan for redirection codes, 
+and uncolored for all other codes.
+*/
+server.use(morgan("dev"));
+server.use(express.json());
+server.use(cors());
+server.use(helmet());
 
 module.exports = server;
