23c3027acb967a590e048fd0680bdfd816187e21
Joseph Luna <josephluna@Josephs-MBP.local>
2019-10-28 23:35:40 -0500

    user migrations file

---
+++ data/migrations/20191028225403_users.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .createTable("users", tbl => {
+      tbl.increments();
+      tbl
+        .text("username")
+        .unique()
+        .notNullable();
+      tbl.text("password").notNullable();
+      tbl.text("email");
+      tbl.decimal("height_cm");
+      tbl.enu('sex', ['male', 'female'], { useNative: true, enumName: 'sex_type' });
+      tbl.date('dob');
+    });
+};
+
+
+exports.down = function(knex) {
+  return knex.schema
+    .dropTableIfExists("users");
+};
