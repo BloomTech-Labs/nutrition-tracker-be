464abc95d94e0cdc42938e714a14a53a16af6b17
Joseph Luna <josephluna@Josephs-MBP.local>
2019-10-29 22:57:47 -0500

    adding consumption log table

---
+++ data/migrations/20191029221802_consumption_log.js
@@ -0,0 +1,31 @@
+
+exports.up = function(knex) {
+  return knex.schema
+    .creatTable("consumption_log", tbl=> {
+        tbl.increments();
+        tbl.integer("user_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("users")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+        tbl.integer("food_bev_id")
+        .unsigned()
+        .notNullable()
+        .references("id")
+        .inTable("food_and_beverages")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+        tbl.datetime("time_consumed_at", { precision: 6 })
+        .defaultTo(knex.fn.now(6));
+        tbl.decimal("human_quantity");
+        tbl.decimal("standard_quantity");
+        tbl.text("unit_type");
+    });
+};
+
+exports.down = function(knex) {
+  return knex.shema
+    .dropTableIfExists("consumption_log");
+};
