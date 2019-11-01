077a5e0028f44ca670cb7a0c2a2244c28e52001e
unknown <will.ediger@gmail.com>
2019-11-01 11:07:28 -0500

    prettier indentation

--- data/migrations/20191029221802_consumption_log.js
+++ data/migrations/20191029221802_consumption_log.js
@@ -1,21 +1,24 @@
 exports.up = function(knex) {
   return knex.schema.createTable("consumption_log", tbl => {
     tbl.increments();
-        tbl.integer("user_id")
+    tbl
+      .integer("user_id")
       .unsigned()
       .notNullable()
       .references("id")
       .inTable("users")
       .onUpdate("CASCADE")
       .onDelete("CASCADE");
-        tbl.integer("food_bev_id")
+    tbl
+      .integer("food_bev_id")
       .unsigned()
       .notNullable()
       .references("id")
       .inTable("food_and_beverages")
       .onUpdate("CASCADE")
       .onDelete("CASCADE");
-        tbl.datetime("time_consumed_at", { precision: 6 })
+    tbl
+      .datetime("time_consumed_at", { precision: 6 })
       .defaultTo(knex.fn.now(6));
     tbl.decimal("human_quantity");
     tbl.decimal("standard_quantity");
