945f00d202cefe1f5496d2a99294adb3bab80c9d
Paul Abernathy <paul.abernathy7@gmail.com>
2019-10-31 21:42:32 -0400

    made fields notnullable

--- data/migrations/20191028225403_users.js
+++ data/migrations/20191028225403_users.js
@@ -1,23 +1,20 @@
-
 exports.up = function(knex) {
-  return knex.schema
-    .createTable("users", tbl => {
-      tbl.increments();
-      tbl
-        .text("username")
-        .unique()
-        .notNullable();
-      tbl.text("password").notNullable();
-      tbl.text("email");
-      tbl.decimal("height_cm");
-      tbl.enu('sex', ['male', 'female'], { useNative: true, enumName: 'sex_type' });
-      tbl.date('dob');
-    });
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
+    tbl
+      .text("username")
+      .unique()
+      .notNullable();
+    tbl.text("password");
+    tbl.text("email").notNullable();
+    tbl.decimal("height_cm").notNullable();
+    tbl
+      .enu("sex", ["male", "female"], { useNative: true, enumName: "sex_type" })
+      .notNullable();
+    tbl.date("dob").notNullable();
+  });
 };
-
 
 exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists("users");
+  return knex.schema.dropTableIfExists("users");
 };
-
