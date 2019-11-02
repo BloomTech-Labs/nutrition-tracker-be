4605d3172def5e0ae767a830e2f171929c9edf37
Alejandro Mejia <aalejandromr@AlejandrosAir2.lan1>
2019-11-01 22:27:54 -0400

    Adds search food endpoint

--- routes/fatsecret/fatsecret.js
+++ routes/fatsecret/fatsecret.js
@@ -15,7 +15,6 @@ router.get('/fatsecret/get-food/:food_id', async (req, res) => {
 
   oathQueryBuilder({ method, food_id: foodID}).get()
     .then(response => {
-      console.log("response", response.params);
       let json = CircularJSON.stringify(response.data);
       res.send(json);
     })
@@ -28,26 +27,14 @@ router.get('/fatsecret/search-food/:search_expression', async (req, res) => {
   const searchExpression = req.params.search_expression;
   const method = 'foods.search';
 
-  const queryParams = [
-    ...getOauthParameters(),
-    ['format', 'json'].join('='),
-    ['max_results', 10].join('='),
-    ['method', method].join('='),
-    ['search_expression', encodeURIComponent(searchExpression)].join('=')
-  ].sort((a, b) => a.localeCompare(b));
-
-  const sha = signRequest(queryParams);
-
-  queryParams.push(['oauth_signature', sha].join('='));
-  axios
-    .get(`${API_PATH}?${queryParams.join('&')}`)
-    .then(response => {
-      let json = CircularJSON.stringify(response);
-      res.send(json);
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  oathQueryBuilder({ method, search_expression: encodeURIComponent(searchExpression), max_results: 10}).get()
+  .then(response => {
+    let json = CircularJSON.stringify(response.data);
+    res.send(json);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
   // res.status(200).send(response);
 });
