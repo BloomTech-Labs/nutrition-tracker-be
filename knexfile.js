8ac20a0dccd1fa966ce2f36f5099cb7dd18cc4cd
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-11-07 22:26:01 -0800

    Added testing environment config to knexfile. Created test folder in data folder for migrations testing. Started building out knex setup function for tests.

--- knexfile.js
+++ knexfile.js
@@ -31,5 +31,19 @@ module.exports = {
     seeds: {
       directory: "./data/seeds"
     }
+  },
+  testing: { 
+    client: "pg",
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
   }
 }
