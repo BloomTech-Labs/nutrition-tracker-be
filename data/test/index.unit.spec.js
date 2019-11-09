66d58d9bbaef224b2596fc5659e5d92da8db12ef
Israel Gonzalez <israelgonzalez@lacroizrules.lan>
2019-11-08 19:28:15 -0800

    All migrations are tested and passing. All expected tables and columns of each table are checked for.

--- data/test/index.unit.spec.js
+++ data/test/index.unit.spec.js
@@ -1,45 +1,99 @@
 /*
-*********
-* 
-* Database test file
-* ✓ Migrations: Make sure all expected tables are being created
-* 
-*
-*********
-*/
+ *********
+ *
+ * Database test file
+ * ✓ Migrations: Make sure all expected tables are being created
+ * ✓ Migrations: Make sure each table has the expected columns
+ *
+ *********
+ */
 
 const knex = require("../knex");
 
 // This arr will hold all the tables we expect to be made on running knex migrate:latest
 const tables = [
-    ["users",[
-        "username", "password","email","height_cm","sex","dob"
-    ]],
-    ["food_and_beverages",[
-        "name", "human_unit","human_quantity","standard_unity","standard_quantity","calories","fat_g","protein_g","carbs_g","sugar_g","fiber_g","sodium_g"
-    ]],
-    ["consumption_log",[
-        "user_id","id","users","food_bev_id","id","food_and_beverages", "time_consumed_at","human_quantity","standard_quantity","unit_type"
-    ]],
-    ["recipes",[
-        "name","description","prep_time_min","cook_time_min","servings","standard_quantity","serving_description"
-    ]],
-    ["recipe_instructions",[
-        "recipe_id","id","recipes","step_number","step_description"
-    ]],
-    ["recipe_ingredients",[
-        "recipe_id","id","users","food_bev_id","id","food_and_beverages","order","human_quantity","standard_quantity","unit_type"
-    ]],
-    ["recipes_consumption",[
-        "user_id","id","users","recipe_id","id","recipes","time_consumed_at","recipe_proportion"
-    ]],
-    ["user_budget_data",[
-        "user_id","id","users","start_date","goal_weekly_weight_change_lb","activity_level","caloric_budget"
-    ]],
-    ["user_metric_history",[
-        "user_id","id","users","observation_time","weight_kg"
-    ]]
-
+  ["users", ["username", "password", "email", "height_cm", "sex", "dob"]],
+  [
+    "food_and_beverages",
+    [
+      "name",
+      "human_unit",
+      "human_quantity",
+      "standard_unit",
+      "standard_quantity",
+      "calories",
+      "fat_g",
+      "protein_g",
+      "carbs_g",
+      "sugar_g",
+      "fiber_g",
+      "sodium_mg"
+    ]
+  ],
+  [
+    "consumption_log",
+    [
+      "user_id",
+      "food_bev_id",
+      "id",
+      "time_consumed_at",
+      "human_quantity",
+      "standard_quantity",
+      "unit_type"
+    ]
+  ],
+  [
+    "recipes",
+    [
+      "name",
+      "description",
+      "prep_time_min",
+      "cook_time_min",
+      "servings",
+      "standard_quantity",
+      "serving_description"
+    ]
+  ],
+  [
+    "recipe_instructions",
+    ["recipe_id", "id", "step_number", "step_description"]
+  ],
+  [
+    "recipe_ingredients",
+    [
+      "recipe_id",
+      "id",
+      "food_bev_id",
+      "id",
+      "order",
+      "human_quantity",
+      "standard_quantity",
+      "unit_type"
+    ]
+  ],
+  [
+    "recipes_consumption",
+    [
+      "user_id",
+      "id",
+      "recipe_id",
+      "id",
+      "time_consumed_at",
+      "recipe_proportion"
+    ]
+  ],
+  [
+    "user_budget_data",
+    [
+      "user_id",
+      "id",
+      "start_date",
+      "goal_weekly_weight_change_lb",
+      "activity_level",
+      "caloric_budget"
+    ]
+  ],
+  ["user_metric_history", ["user_id", "id", "observation_time", "weight_kg"]]
 ];
 
 // create a function to setup our migrations
@@ -53,11 +107,15 @@ const setup = async () => {
 
 // Trunc func in case we want to to test tables in isolation
 const truncate = async () => {
-    // return a concat'd string of all the tables in tables array at the top of this file
-    const tablesToTrunc = tables.length > 1 ? tables.join() : tables[0];
+  // return a concat'd string of all the tables in tables array at the top of this file
+  const concatTables = [];
+  for (const table in tables) {
+    concatTables.push(table[0]);
+  }
+  const tablesToTrunc = tables.length > 1 ? concatTables.join() : tables[0][0];
 
-    // run the raw sql command using the concat'd tables array
-    await knex.raw("truncate table " + tablesToTrunc + " cascade");
+  // run the raw sql command using the concat'd tables array
+  await knex.raw("truncate table " + tablesToTrunc + " cascade");
 };
 
 // Rollback our migrations so we start fresh
@@ -70,7 +128,6 @@ beforeAll(async () => {
   await teardown();
   //jest.setTimeout(5000);
   await setup();
-  
 });
 
 // If we need to, we can teardown after all tests but might not have to
@@ -78,35 +135,35 @@ beforeAll(async () => {
 //   await teardown();
 // });
 
-
 describe.each`
-    table             | expected
-    ${"fakeTable"}    | ${false}
-    ${tables[0]}      | ${true}
-    ${tables[1]}      | ${true}
-    ${tables[2]}      | ${true}
-    ${tables[3]}      | ${true}
-    ${tables[4]}      | ${true}
-    ${tables[5]}      | ${true}
-    ${tables[6]}      | ${true}
-    ${tables[7]}      | ${true}
-    ${tables[8]}      | ${true}
-`("let's check out the table named $table", ({table, expected}) => {
-  test("returns $expected when we look for a table named $table", async () => {
+  table        | expected
+  ${tables[0]} | ${true}
+  ${tables[1]} | ${true}
+  ${tables[2]} | ${true}
+  ${tables[3]} | ${true}
+  ${tables[4]} | ${true}
+  ${tables[5]} | ${true}
+  ${tables[6]} | ${true}
+  ${tables[7]} | ${true}
+  ${tables[8]} | ${true}
+`(`let's check out the table named $table`, ({ table, expected }) => {
+  let tableExists = [];
+  test(`returns ${expected} when we look for a table named ${
+    table[0]
+  }`, async () => {
     expect(
-        await knex.schema.hasTable(table[0]).then(exists => {
-            /*exists //? console.log('success') : console.log("fail");
-             ? table[1].forEach(column=0; table[1].length > column; column++){
-                test(`${table} has a column named ${table[1][column]}`, () => {
-                    await knex.schema.hasColumn(table[0], table[1][column]).then(hasColumn => {
-                        return hasColumn;
-                    })
-                }).toBe(true);
-            }
-            : null; */  // For the above, trying to create dynamic tests for each column of each table!
-          return exists;
-        })
-      ).toBe(expected);
-  })
-  
+      await knex.schema.hasTable(table[0]).then(exists => {
+        tableExists = exists;
+        return exists;
+      })
+    ).toBe(expected);
+  });
+
+  test.each(table[1])(`Does the column %s exist?`, async column => {
+    expect(
+      await knex.schema.hasColumn(table[0], column).then(hasColumn => {
+        return hasColumn;
+      })
+    ).toBe(expected);
+  });
 });
