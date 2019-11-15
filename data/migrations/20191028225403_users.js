470ae7a2d29cdb88d41c8fde8a1f252c7ce3f5b6
unknown <will.ediger@gmail.com>
2019-11-15 16:03:21 -0600

    removed unit_system_type from migrations

--- data/migrations/20191028225403_users.js
+++ data/migrations/20191028225403_users.js
@@ -1,26 +1,22 @@
 exports.up = function(knex) {
-  return knex.schema.createTable('users', tbl => {
+  return knex.schema.createTable("users", tbl => {
     tbl.increments();
     tbl
-      .text('firebase_id')
+      .text("firebase_id")
       .unique()
       .notNullable();
-    tbl.text('email');
-    tbl.decimal('height_cm')
-      .notNullable()
-    tbl.enu('sex', ['male', 'female'], {
-      useNative: true,
-      enumName: 'sex_type'
-    })
-    .notNullable()
-    tbl.date('dob')
-      .notNullable()
+    tbl.text("email");
+    tbl.decimal("height_cm").notNullable();
+    tbl
+      .enu("sex", ["male", "female"], {
+        useNative: true,
+        enumName: "sex_type"
+      })
+      .notNullable();
+    tbl.date("dob").notNullable();
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists('users')
-    .raw('DROP TYPE sex_type;')
-    .raw('DROP TYPE unit_system_type;');
+  return knex.schema.dropTableIfExists("users").raw("DROP TYPE sex_type;");
 };
