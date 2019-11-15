f474a02c8493ce3ec401eb245956f598957daf0b
unknown <will.ediger@gmail.com>
2019-11-14 18:19:31 -0600

    added macro target ratios

--- data/migrations/20191101115603_user_budget_data.js
+++ data/migrations/20191101115603_user_budget_data.js
@@ -1,21 +1,24 @@
 exports.up = function(knex) {
-  return knex.schema.createTable("user_budget_data", tbl => {
+  return knex.schema.createTable('user_budget_data', tbl => {
     tbl.increments();
     tbl
-      .integer("user_id")
+      .integer('user_id')
       .unsigned()
       .notNullable()
-      .references("id")
-      .inTable("users")
-      .onUpdate("CASCADE")
-      .onDelete("CASCADE");
-    tbl.date("start_date");
-    tbl.decimal("goal_weekly_weight_change_lb");
-    tbl.decimal("activity_level");
-    tbl.decimal("caloric_budget");
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl.date('start_date');
+    tbl.decimal('goal_weekly_weight_change_lb');
+    tbl.decimal('activity_level');
+    tbl.decimal('caloric_budget');
+    tbl.float('fat_ratio');
+    tbl.float('protein_ratio');
+    tbl.float('carb_ratio');
   });
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("user_budget_data");
+  return knex.schema.dropTableIfExists('user_budget_data');
 };
