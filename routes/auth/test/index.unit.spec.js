a4cfdb41629fd1b38ee004cb5d2de516ef4fac4c
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-17 11:10:26 -0500

    adds tests for the auth/register endpoint

---
+++ routes/auth/test/index.unit.spec.js
@@ -0,0 +1,77 @@
+const app = require('../../../server');
+const db = require('../../../data/knex.js');
+const supertest = require('supertest');
+const request = supertest(app);
+
+describe('authRouter.js', () => {
+  it('should set the testing environment', () => {
+    expect(process.env.DB_ENV).toBe('testing');
+  });
+
+  describe('POST /auth/register', () => {
+    beforeEach(async () => {
+      await db.truncate('user_metric_history');
+      await db.truncate('user_budget_data');
+      await db.raw('TRUNCATE TABLE users RESTART IDENTITY CASCADE');
+    });
+
+    // Valid Request Body
+    const requestBody_1 = {
+      firebase_id: 'sDs3omvkWje9Lgv8wEteFkhZpVa2',
+      sex: 'male',
+      activity_level: 1.2,
+      dob: '1987-09-29T00:00:00.000Z',
+      weight_kg: 81.65,
+      height_cm: 160,
+      weekly_goal_rate: -1,
+      email: 'user1@email.com'
+    };
+
+    // Request Body with duplicate firebase_id
+    const requestBody_2 = {
+      firebase_id: 'sDs3omvkWje9Lgv8wEteFkhZpVa2', // duplicate id
+      sex: 'female',
+      activity_level: 1.9,
+      dob: '1995-08-04T00:00:00.000Z',
+      weight_kg: 61,
+      height_cm: 140,
+      weekly_goal_rate: 1,
+      email: 'user1@email.com'
+    };
+
+    // Malformed Request Body with missing fields
+    const requestBody_1_malformed = {
+      firebase_id: 'sDs3omvkWje9Lgv8wEteFkhZpVa2',
+      sex: 'male',
+      activity_level: 1.2,
+      dob: '1987-09-29T00:00:00.000Z',
+      weight_kg: 81.65,
+      //height_cm: 160,
+      weekly_goal_rate: -1,
+      email: 'user1@email.com'
+    };
+
+    it('should return a json object', async () => {
+      const res = await request.post('/auth/register').send(requestBody_1);
+      expect(res.type).toBe('application/json');
+    });
+
+    it('should return 201 Created response', async () => {
+      const res = await request.post('/auth/register').send(requestBody_1);
+      expect(res.status).toBe(201);
+    });
+
+    it('should return 400 Bad Request response', async () => {
+      const res = await request
+        .post('/auth/register')
+        .send(requestBody_1_malformed); // with missing fields
+      expect(res.status).toBe(400);
+    });
+
+    it('should return 500 Internal Server response', async () => {
+      await request.post('/auth/register').send(requestBody_1);
+      const res = await request.post('/auth/register').send(requestBody_2); // with duplicate firebase_id
+      expect(res.status).toBe(500);
+    });
+  });
+});
