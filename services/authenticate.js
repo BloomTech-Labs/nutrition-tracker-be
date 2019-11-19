ac7d1f223d90112e3411ffb04c289cdeb4382670
Dave Vazquez <dmvaz.li@gmail.com>
2019-11-19 09:56:16 -0500

    initializes middleware for fireb token validation

---
+++ services/authenticate.js
@@ -0,0 +1,31 @@
+const admin = require('./firebaseAdmin');
+
+const getAuthToken = (req, res, next) => {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === 'Bearer'
+  ) {
+    req.authToken = req.headers.authorization.split(' ')[1];
+  } else {
+    req.authToken = null;
+  }
+  next();
+};
+
+
+module.exports = (req, res, next) => {
+ getAuthToken(req, res, async () => {
+    try {
+      const { authToken } = req;
+      const userInfo = await admin
+        .auth()
+        .verifyIdToken(authToken);
+      req.firebase_id = userInfo.uid;
+      return next();
+    } catch (err) {
+      return res
+        .status(401)
+        .send({ error: 'You are not authorized to make this request' });
+    }
+  });
+};
