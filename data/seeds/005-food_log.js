d2f1aff3263d6a7fb9465f54306a8e3e3fbf3942
Joseph Luna <josephluna@Josephs-MBP.local>
2019-11-21 23:20:05 -0600

    updated food-log seeds  table and table fileds to reflect db table
    co-authored-by: Paul<paul.abernathy7@gmail.com>

--- data/seeds/005-food_log.js
+++ data/seeds/005-food_log.js
@@ -1,73 +1,77 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("consumption_log")
+  return knex("food_log")
     .truncate()
     .then(function() {
       // Inserts seed entries
-      return knex("consumption_log").insert([
+      return knex("food_log").insert([
         {
           id: 1,
           user_id: 2,
-          food_bev_id: 1,
+          food_id: 1,
+          fatsecret_food_id:9876,
+          serving_id:10,
           time_consumed_at: knex.fn.now(6),
-          human_quantity: 2,
-          standard_quantity: 0.0,
-          unit_type: ""
+          quantity: 2.0,
         },
         {
           id: 2,
           user_id: 3,
-          food_bev_id: 1,
+          food_id: 2,
+          fatsecret_food_id:8765,
+          serving_id:11,
           time_consumed_at: knex.fn.now(6),
-          human_quantity: 1,
-          standard_quantity: 0.0,
-          unit_type: ""
+          quantity: 1.0,
         },
         {
           id: 3,
           user_id: 3,
-          food_bev_id: 2,
+          food_id: 3,
+          fatsecret_food_id:7654,
+          serving_id:12,
           time_consumed_at: knex.fn.now(6),
-          human_quantity: 2,
-          standard_quantity: 0.0,
-          unit_type: ""
+          quantity: 4.0,
         },
         {
           id: 4,
           user_id: 2,
-          food_bev_id: 4,
+          food_id: 4,
+          fatsecret_food_id:6543,
+          serving_id:13,
           time_consumed_at: knex.fn.now(6),
-          human_quantity: 1,
-          standard_quantity: 0.0,
-          unit_type: ""
+          quantity: 2.0,
         },
         {
           id: 5,
           user_id: 4,
-          food_bev_id: 3,
+          food_id: 2,
+          fatsecret_food_id:5432,
+          serving_id:10,
           time_consumed_at: knex.fn.now(6),
-          human_quantity: 1,
-          standard_quantity: 0.0,
-          unit_type: ""
+          quantity: 2.0,
         },
         {
           id: 6,
           user_id: 1,
-          food_bev_id: 2,
+          food_id: 1,
+          fatsecret_food_id:4321,
+          serving_id:14,
           time_consumed_at: knex.fn.now(6),
-          human_quantity: 3,
-          standard_quantity: 0.0,
-          unit_type: ""
+          quantity: 1.0,
         },
         {
           id: 7,
           user_id: 1,
-          food_bev_id: 2,
+          food_id: 1,
+          fatsecret_food_id:3210,
+          serving_id:15,
           time_consumed_at: knex.fn.now(6),
-          human_quantity: 3,
-          standard_quantity: 0.0,
-          unit_type: ""
+          quantity: 1.5,
         }
       ]);
     });
 };
+
+
+//In the foods table we need to reflect the FK for food_id to be foods_id and update the DB Designer
+//Need to update this table name in the cleanup.js file
