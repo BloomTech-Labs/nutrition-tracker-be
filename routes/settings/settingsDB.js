43e2a998607e8b7828f4e10784dd131c2e9f814d
Chaz Rowe <chazr_88@hotmail.com>
2019-11-21 22:43:49 -0500

    updated endpoints

--- routes/settings/settingsDB.js
+++ routes/settings/settingsDB.js
@@ -1,10 +1,25 @@
 const db = require("../../data/knex");
 
 module.exports = {
-    find
+    find,
+    findById,
+    updateUserSettings
 }
 
-//Returns all bucket lists
+
 function find() {
 	return db('users');
+}
+
+function findById(id) {
+	return db("users")
+		.where({ id })
+		.first();
+}
+
+async function updateUserSettings(updates, id) {
+	await db("users")
+		.where({ id })
+		.update(updates);
+	return findById(id);
 }
--- routes/settings/settingsRouter.js
+++ routes/settings/settingsRouter.js
@@ -11,9 +11,44 @@ router.get("/", async (req, res) => {
 	}
 });
 
+router.get("/:id", async (req, res) => {
+	const { id } = req.params;
+	try {
+		const user = await UserInfo.findById(id);
+		user.height = heightToImperial(user.height_cm)
 
-router.put('/', async (req, res) => {
+		res.json(user);
+	} catch (err) {
+		res.status(500).json({ err });
+	}
+});
 
-})
+router.put("/:id", async (req, res) => {
+	const id = req.params.id;
+	const updatedSettings = req.body;
+	if(!updatedSettings) {
+		res.status(400).json({
+			message: "Item required for update are missing"
+		})
+	}
+	try {
+		const updated = await UserInfo.updateUserSettings(updatedSettings, id);
+		res.status(201).json(updated);
+	} catch (err) {
+		res.status(500).json({ message: "Failed to update user settings" });
+	}
+});
+
+function heightToImperial(n){
+	const height = []
+	var realFeet = ((n*0.393700) / 12);
+	var convFeet = Math.floor(realFeet);
+	var convInches = Math.round((realFeet - convFeet) * 12);
+	height.push({
+		feet: convFeet,
+		inches: convInches
+	})
+	return height
+}
 
 module.exports = router;
