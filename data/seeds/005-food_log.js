58fd6b5fcee9a224028fb9421c6ba129abfcff84
Paul Abernathy <paul.abernathy7@gmail.com>
2019-11-23 15:46:02 -0500

    updated recipe_ingredients
    Co-authored-by:joe<joeluna17@yahoo.com>

--- data/seeds/005-food_log.js
+++ data/seeds/005-food_log.js
@@ -9,69 +9,68 @@ exports.seed = function(knex) {
           id: 1,
           user_id: 2,
           food_id: 1,
-          fatsecret_food_id:9876,
-          serving_id:10,
+          fatsecret_food_id: 9876,
+          serving_id: 10,
           time_consumed_at: knex.fn.now(6),
-          quantity: 2.0,
+          quantity: 2.0
         },
         {
           id: 2,
           user_id: 3,
           food_id: 2,
-          fatsecret_food_id:8765,
-          serving_id:11,
+          fatsecret_food_id: 8765,
+          serving_id: 11,
           time_consumed_at: knex.fn.now(6),
-          quantity: 1.0,
+          quantity: 1.0
         },
         {
           id: 3,
           user_id: 3,
           food_id: 3,
-          fatsecret_food_id:7654,
-          serving_id:12,
+          fatsecret_food_id: 7654,
+          serving_id: 12,
           time_consumed_at: knex.fn.now(6),
-          quantity: 4.0,
+          quantity: 4.0
         },
         {
           id: 4,
           user_id: 2,
           food_id: 4,
-          fatsecret_food_id:6543,
-          serving_id:13,
+          fatsecret_food_id: 6543,
+          serving_id: 13,
           time_consumed_at: knex.fn.now(6),
-          quantity: 2.0,
+          quantity: 2.0
         },
         {
           id: 5,
           user_id: 4,
           food_id: 2,
-          fatsecret_food_id:5432,
-          serving_id:10,
+          fatsecret_food_id: 5432,
+          serving_id: 14,
           time_consumed_at: knex.fn.now(6),
-          quantity: 2.0,
+          quantity: 2.0
         },
         {
           id: 6,
           user_id: 1,
           food_id: 1,
-          fatsecret_food_id:4321,
-          serving_id:14,
+          fatsecret_food_id: 4321,
+          serving_id: 15,
           time_consumed_at: knex.fn.now(6),
-          quantity: 1.0,
+          quantity: 1.0
         },
         {
           id: 7,
           user_id: 1,
           food_id: 1,
-          fatsecret_food_id:3210,
-          serving_id:15,
+          fatsecret_food_id: 3210,
+          serving_id: 16,
           time_consumed_at: knex.fn.now(6),
-          quantity: 1.5,
+          quantity: 1.5
         }
       ]);
     });
 };
-
 
 //In the foods table we need to reflect the FK for food_id to be foods_id and update the DB Designer
 //Need to update this table name in the cleanup.js file
