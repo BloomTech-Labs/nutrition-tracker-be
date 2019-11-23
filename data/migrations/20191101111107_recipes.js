10cf6647ec7a6b73b5b44fcf92b1b88c0811b6e2
Paul Abernathy <paul.abernathy7@gmail.com>
2019-11-23 18:28:14 -0500

    removed cascade dquote> dquote> dquote> Co-authored-by: joe<joeluna17@yahoo.com>

--- data/migrations/20191101111107_recipes.js
+++ data/migrations/20191101111107_recipes.js
@@ -16,5 +16,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists("recipes").cascade;
+  return knex.schema.dropTableIfExists("recipes");
 };
