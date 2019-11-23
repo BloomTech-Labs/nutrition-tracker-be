7a1268191473794b90479dbc7a801a230828ae15
Paul Abernathy <paul.abernathy7@gmail.com>
2019-11-23 15:59:26 -0500

    updated recipes_log
    Co-authored-by: joe<joeluna17@yahoo.com>

--- data/seeds/009-recipes_log.js
+++ data/seeds/009-recipes_log.js
@@ -9,6 +9,7 @@ exports.seed = function(knex) {
           id: 1,
           user_id: 1,
           recipe_id: 2,
+          fatsecret_recipe_id: 0,
           time_consumed_at: knex.fn.now(6),
           recipe_proportion: 2.0
         },
@@ -16,6 +17,7 @@ exports.seed = function(knex) {
           id: 2,
           user_id: 2,
           recipe_id: 2,
+          fatsecret_recipe_id: 0,
           time_consumed_at: knex.fn.now(6),
           recipe_proportion: 1.0
         },
@@ -23,6 +25,7 @@ exports.seed = function(knex) {
           id: 3,
           user_id: 1,
           recipe_id: 1,
+          fatsecret_recipe_id: 0,
           time_consumed_at: knex.fn.now(6),
           recipe_proportion: 1.5
         },
@@ -30,6 +33,7 @@ exports.seed = function(knex) {
           id: 4,
           user_id: 3,
           recipe_id: 2,
+          fatsecret_recipe_id: 0,
           time_consumed_at: knex.fn.now(6),
           recipe_proportion: 1.0
         },
@@ -37,6 +41,7 @@ exports.seed = function(knex) {
           id: 5,
           user_id: 1,
           recipe_id: 1,
+          fatsecret_recipe_id: 0,
           time_consumed_at: knex.fn.now(6),
           recipe_proportion: 2.0
         },
@@ -44,6 +49,7 @@ exports.seed = function(knex) {
           id: 6,
           user_id: 1,
           recipe_id: 2,
+          fatsecret_recipe_id: 0,
           time_consumed_at: knex.fn.now(6),
           recipe_proportion: 1.0
         }
