647305770895a3f514f0db705a3f484135409ee3
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-01 23:54:09 -0500

    comments out seed data

--- data/seeds/002-user-metric-history.js
+++ data/seeds/002-user-metric-history.js
@@ -2,48 +2,6 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("user_metric_history")
     .truncate()
-<<<<<<< HEAD
-    .then(function() {
-      // Inserts seed entries
-      return knex("user_metric_history").insert([
-        {
-          id: 1,
-          user_id: 1,
-          weight_kg: 81.6466
-        },
-        {
-          id: 2,
-          user_id: 3,
-          weight_kg: 72.5748
-        },
-        {
-          id: 3,
-          user_id: 2,
-          weight_kg: 86.1826
-        },
-        {
-          id: 4,
-          user_id: 4,
-          weight_kg: 70.3068
-        },
-        {
-          id: 5,
-          user_id: 2,
-          weight_kg: 85.729
-        },
-        {
-          id: 6,
-          user_id: 3,
-          weight_kg: 72.1212
-        },
-        {
-          id: 7,
-          user_id: 1,
-          weight_kg: 81.193
-        }
-      ]);
-    });
-=======
     // .then(function() {
     //   // Inserts seed entries
     //   return knex("user_metric_history").insert([
@@ -84,5 +42,4 @@ exports.seed = function(knex) {
     //     }
     //   ]);
     // });
->>>>>>> settings
 };
