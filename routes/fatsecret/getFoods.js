0a2f40823dfa3510cc5795f0c11aa314b7b05e56
unknown <will.ediger@gmail.com>
2019-12-03 16:42:11 -0600

    refactored hard coded millisecond calculation to using modifyable constant of "22" hours

--- routes/fatsecret/getFoods.js
+++ routes/fatsecret/getFoods.js
@@ -6,10 +6,11 @@ module.exports = {
 };
 
 function getServingsByFatsecretFoodId(fatsecret_food_id) {
+  const fatsecretCacheLimitHours = 22;
   // calculate current time, minus 22 hours -- to make sure we dont overlap with fatsecret's cache time length maximum of 24 hours.
-  // 22h = 1000ms*60s*60min*22hrs = 79200000 = 79.2*10**6 = 79.2 million ms
-  const oneMillion = 10 ** 6;
-  const cacheCutoff = new Date(Date.now() - 79.2 * oneMillion);
+  // 22h = 1000ms*60s*60min*22hrs = 79200000 = 79.2 million ms = 79.2*10**6
+  const fatsecretCacheLimitMs = 1000 * 60 * 60 * fatsecretCacheLimitHours;
+  const cacheCutoff = new Date(Date.now() - fatsecretCacheLimitMs);
   // use moment to calc 22 hours before now, to use pass to .where() clause
   return db("foods as f")
     .select("f.*")
