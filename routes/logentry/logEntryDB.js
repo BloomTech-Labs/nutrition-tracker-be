b2ab20704e3da381e07ffb4f7ab914067b9ef41b
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-03 21:42:14 -0500

    renamea to logEntryDB

---
+++ routes/logentry/logEntryDB.js
@@ -0,0 +1,18 @@
+const db = require("../../data/knex");
+
+module.exports = {
+  addLogEntry,
+  removeLogEntry
+};
+
+function addLogEntry(log_entry) {
+  return db("food_log")
+    .insert(log_entry)
+    .returning("*");
+}
+
+function removeLogEntry(id) {
+  return db("food_log")
+  .delete()
+  .where({id})
+}
