a076514c6ebf2a09ec5f898a392f0b4b984fb87b
unknown <will.ediger@gmail.com>
2019-12-07 19:50:08 -0600

    built out upsert functionality using pg-promise

---
+++ routes/fatsecret/upsertFoods.js
@@ -0,0 +1,57 @@
+const pgp = require("pg-promise")();
+const dbConnection = process.env.DATABASE_URL;
+
+const db = pgp(dbConnection);
+
+module.exports = { upsertFoods };
+
+function upsertFoods(data) {
+  const query = upsertQuery(data);
+  console.log(query);
+  try {
+    return db.any(query);
+  } catch (e) {
+    return e;
+  }
+}
+
+function upsertQuery(data) {
+  const columnSet = new pgp.helpers.ColumnSet(
+    [
+      "fatsecret_food_id",
+      "serving_id",
+      "food_name",
+      "serving_url",
+      "serving_desc",
+      "metric_serving_amt",
+      "metric_serving_unit",
+      "calories_kcal",
+      "fat_g",
+      "carbs_g",
+      "protein_g",
+      "saturated_fat_g",
+      "monounsaturated_fat_g",
+      "polyunsaturated_fat_g",
+      "trans_fat_g",
+      "fiber_g",
+      "sugar_g",
+      "cholesterol_mg",
+      "sodium_mg",
+      "potassium_mg",
+      "vitamin_a_daily_pct",
+      "vitamin_c_daily_pct",
+      "calcium_daily_pct",
+      "iron_daily_pct"
+    ],
+    { table: "foods" }
+  );
+  return (
+    pgp.helpers.insert(data, columnSet) +
+    " ON CONFLICT(fatsecret_food_id, serving_id) DO UPDATE SET " +
+    columnSet.assignColumns({
+      from: "EXCLUDED",
+      skip: ["id", "fatsecret_food_id", "serving_id"]
+    }) +
+    " RETURNING *"
+  );
+}
