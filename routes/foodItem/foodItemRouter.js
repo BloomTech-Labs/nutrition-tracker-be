704c3b75893a0658d3d088bde87c5d73f7050734
joeluna17 <joeluna17@yahoo.com>
2019-12-18 23:17:53 -0600

    updated info
    Co-authored-by: Paul Abernathy <paul.abernathy7@gmail.com>

--- routes/foodItem/foodItemRouter.js
+++ routes/foodItem/foodItemRouter.js
@@ -1,43 +1,46 @@
 const express = require("express");
 const db = require("./foodItemDB");
 const fetch = require("node-fetch");
+const mapFirebaseIDtoUserID = require("../../middleware/mapFirebaseIDtoUserID");
 
 router = express.Router();
 
-router.get("/getfooditem/:foodlogID/user/:id", async (req, res) => {
-  const { id } = req.params;
-  const { foodlogID } = req.params;
-
-  const checkStatus = res => {
-    //Error Check
-    if (res.ok) {
-      return res;
-    } else {
-      throw new Error(`Request Failed: ${res.statusText}`);
-    }
-  };
-
-  try {
-    const [foodItem] = await db.getFoodItem(foodlogID, id);
-    const { fatsecret_food_id } = foodItem;
-    //now that we have our data from the db we need to go and get the fatsecret_food_id for this record and return that info.
-    try {
-      var data;
-      await fetch(
-        `http://localhost:4000/fatsecret/get-food/${fatsecret_food_id}` //make a fetch request from our api and and get info
-      )
-        .then(checkStatus)
-        .then(res => res.json())
-        .then(json => (data = json)); // json is the actaul data being returned from out api call
-      const dataAtIndexOne = data[1];
-      res.status(200).json({ ...dataAtIndexOne, ...foodItem }); // return the api call json data and combine with local db data
-    } catch ({ message }) {
-      res.status(404).json(message);
-    }
-    //now we that we have that data from the end point we we need to spread in out data from the first call
-  } catch ({ message }) {
-    res.status(500).json(message);
+const checkStatus = res => {
+  //Error Check
+  if (res.ok) {
+    return res;
+  } else {
+    throw new Error(`Request Failed: ${res.statusText}`);
   }
-});
+};
+
+router.get(
+  "/getfooditem/:foodlogID/user/:user_id",
+  mapFirebaseIDtoUserID,
+  async (req, res) => {
+    const { user_id, foodlogID } = req.params;
+    try {
+      const [foodItem] = await db.getFoodItem(foodlogID, user_id);
+      const { fatsecret_food_id } = foodItem;
+      //now that we have our data from the db we need to go and get the fatsecret_food_id for this record and return that info.
+      try {
+        var data;
+        await fetch(
+          `http://localhost:4000/fatsecret/get-food/${fatsecret_food_id}` //make a fetch request from our api and and get info
+        )
+          .then(checkStatus)
+          .then(res => res.json())
+          .then(json => (data = json)); // json is the actaul data being returned from out api call
+        const dataAtIndexOne = data[1];
+        res.status(200).json({ ...dataAtIndexOne, ...foodItem }); // return the api call json data and combine with local db data
+      } catch ({ message }) {
+        res.status(404).json(message);
+      }
+      //now we that we have that data from the end point we we need to spread in out data from the first call
+    } catch ({ message }) {
+      res.status(500).json(message);
+    }
+  }
+);
 
 module.exports = router;
