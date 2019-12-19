bd47fab48be45c94e01ce964956eaa725fc822b1
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-19 00:54:41 -0500

    adds helper function to recalculate caloric budget

--- routes/settings/helper.js
+++ routes/settings/helper.js
@@ -1,4 +1,4 @@
-const moment = require("moment-timezone");
+
 //Converts cm to feet and inches.
 function heightToImperial(n) {
   var realFeet = n * 0.3937 / 12;
@@ -17,7 +17,45 @@ function kgToLbs(kg) {
   return lbs;
 }
 
+/********************************************************
+ *                   GET CALORIC BUDGET                  *
+ ********************************************************/
+// Calculates a user's caloric budget using the
+// Mifflin-St. Jeor Equation for BMR (Basal Metabolic Rate)
+// mutiplied by an Activity Factor of (1.2 - 1.9)
+// https://www.calculator.net/bmr-calculator.html
+function recalculateCaloricBudget(newUser) {
+  let { sex, activity_level, dob, actual_weight_kg, height_cm } = newUser;
+
+  return Math.round((
+    (10 * actual_weight_kg) 
+    + (6.25 * height_cm) 
+    - (5 * getAge(dob)) 
+    + (sex === "Male" ? 5 : -161)) 
+    * activity_level
+  );
+}
+
+/********************************************************
+ *                        GET AGE                        *
+ ********************************************************/
+function getAge(dob) {
+  const today = new Date();
+  const birthDate = new Date(dob);
+  const age = today.getFullYear() - birthDate.getFullYear();
+  const monthDifference = today.getMonth() - birthDate.getMonth();
+  const dayDifference = today.getDate() - birthDate.getDate();
+  // handles edge case where if the user's birth month or birth day
+  // falls after today's date, if that's the case decrement the age by 1
+  // because they haven't yet hit their birthday
+  if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+    age--;
+  }
+  return age;
+}
+
 module.exports = {
   heightToImperial,
-  kgToLbs
+  kgToLbs,
+  recalculateCaloricBudget
 };
