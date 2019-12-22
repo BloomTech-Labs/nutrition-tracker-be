c6cef767c76487db543146a065cab388634d51bd
Will Ediger <will.ediger@gmail.com>
2019-12-22 01:46:52 -0600

    updateWeightGoal() pseudocode implementation, culminating in update_weight_goal.js

---
+++ routes/settings/sql/days_until_goal.js
@@ -0,0 +1,19 @@
+module.exports = days_until_goal = user_id => {
+  //subquery strings, with alias; should just need comma as if they're just a "field name" in terms of building a sql string.
+  const actual_weight_kg = require("./current_values/actual_weight_kg")(user_id);
+  const goal_weekly_weight_change_rate_kg = require("./current_values/goal_weekly_weight_change_rate_kg")(user_id);
+  const goal_weight_kg = require("./current_values/goal_weight_kg")(user_id);
+
+  const result = `
+    select 
+      ceil(abs(v.actual_weight_kg - v.goal_weight_kg) / v.goal_weekly_weight_change_rate_kg * 7) as days_until_goal
+    from ( 
+      select 
+        ${actual_weight_kg}, 
+        ${goal_weight_kg}, 
+        ${goal_weekly_weight_change_rate_kg}
+    ) as v
+  `;
+
+  return result;
+};
