2a6740cb93ea356b639488c8f0588dbb5b5c72da
Will Ediger <will.ediger@gmail.com>
2019-12-22 03:51:17 -0600

    update_weight_goal logic moved into update_goal_date_if_necessary, as the "if_necessary" piece is done easily in the same step

--- routes/settings/sql/update_goal_date_if_necessary.js
+++ routes/settings/sql/update_goal_date_if_necessary.js
@@ -1,3 +1,24 @@
-const update_goal_date_if_necessary = () => {};
+const update_goal_date_if_necessary = (user_id, time_zone) => {
+  //subquery strings, with alias; should just need comma as if they're just a "field name" in terms of building a sql string.
+  const days_until_goal = require("./days_until_goal")(user_id);
+  const days_until_goal = require("./days_until_goal")(user_id);
+
+  const result = `
+    insert into user_budget_data(user_id, goal_start_date, goal_end_date)
+    select 
+      ${user_id} as user_id,
+      timezone('${time_zone}', current_date)::date as new_goal_start_date,
+      (timezone('${time_zone}', current_date) + (v.days_until_goal * interval '1 day'))::date as new_goal_end_date
+    from ( 
+      ${days_until_goal}
+    ) as v
+    inner join ${needs_new_goal}
+    on 
+      v.user_id = needs_new_goal.user_id
+    returning *;
+  `;
+
+  return result;
+};
 
 module.exports = update_goal_date_if_necessary;
