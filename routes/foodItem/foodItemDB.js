0ff9b081778266f7926f30cc7b4058b87453a86f
joeluna17 <joeluna17@yahoo.com>
2019-12-21 22:45:22 -0600

    successfully updateing food-item-quantity moving on to date / time

--- routes/foodItem/foodItemDB.js
+++ routes/foodItem/foodItemDB.js
@@ -3,7 +3,7 @@ const db = require("../../data/knex");
 
 const getFoodItem = async (foodLogID, userID) => {
     try{
-        const log_data = await db("food_log as fl").where({ id: foodLogID, user_id: userID });    
+        const log_data = await db("food_log").where({ id: foodLogID, user_id: userID });    
             return log_data ;
          }
          catch(err){
@@ -16,7 +16,7 @@ const updateFoodItem = async (foodLogId, userID, foodItem) => {
     //update the record with the updated informnation
     console.log('IN THE BACK END HERE IS THE ENDPOINT FOR UPDATE ITEM', foodItem)
     try{
-       const updatedRecord = await db("food_log").where({id:foodLogId, user_id:userID}).update(foodItem)
+       const updatedRecord = await db("food_log").where({ id:foodLogId, user_id:userID }).update({"quantity": foodItem.updatedQuantity});
        return updatedRecord;
     }catch(err){
         return err;
