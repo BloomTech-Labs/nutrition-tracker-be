1abdff0d963d4722edff4eb8b0de4963f1b32b36
Will Ediger <will.ediger@gmail.com>
2019-12-23 01:23:17 -0600

    added custom "set values" logic variant of UPSERT sql creator

--- data/helpers/upsert.js
+++ data/helpers/upsert.js
@@ -22,4 +22,24 @@ const createUpsertQuerySql = (
   );
 };
 
-module.exports = { createUpsertQuerySql };
+//generates *sql string* for an UPSERT functionality
+const createUpsertQueryCustomSetValuesLogicSql = (
+  data, // array of objects to be upserted
+  table, // name of table we're upserting to
+  columns, // array of columns to be inserted/updated
+  onConflictColumns, // set of columns or constraint that defines whether to update or insert
+  setColumnsStr // string that details the values that you want to set if it's an update
+  // rather than an insert
+) => {
+  const onConflictColumnsStr = onConflictColumns.join(", ");
+
+  const columnSet = new pgp.helpers.ColumnSet(columns, { table: table });
+  return (
+    pgp.helpers.insert(data, columnSet) +
+    ` ON CONFLICT(${onConflictColumnsStr}) DO UPDATE SET ` +
+    setColumnsStr +
+    " RETURNING *"
+  );
+};
+
+module.exports = { createUpsertQuerySql, createUpsertQueryAddValuesSql: createUpsertQueryCustomSetValuesLogicSql };
