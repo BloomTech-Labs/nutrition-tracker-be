c969f5a5d07e9ccde68e4581c72140ff4b0a23a0
Will Ediger <will.ediger@gmail.com>
2019-12-23 01:24:15 -0600

    changed daily totals to increment/decrement the table by the given amount instead of replacing it

--- routes/dailyTotals/dailyTotalsDB.js
+++ routes/dailyTotals/dailyTotalsDB.js
@@ -2,34 +2,23 @@
 // not using knex because knex doesn't like sending a raw sql query if it has upserting
 const { db } = require("../../data/pg-promise.js");
 
-const { createUpsertQuerySql } = require("../../data/helpers/upsert.js");
+const { createUpsertQueryCustomSetValuesLogicSql } = require("../../data/helpers/upsert.js");
 
 const upsertDailyTotal = data => {
   const table = "daily_nutrition_totals";
 
-  const setColumns = [
-    "total_calories",
-    "fat_calories",
-    "carbs_calories",
-    "protein_calories"
-  ];
+  const setColumns = ["total_calories", "fat_calories", "carbs_calories", "protein_calories"];
+
+  const lineStr = e => `${e}=${e}+EXCLUDED.${e}`;
+  const arrLines = setColumns.map(lineStr);
+  const setColumnsStr = arrLines.join(",");
 
   const onConflictColumns = ["user_id", "date"];
 
-  const skipInsertColumns = ["id"];
-
   const columns = [...onConflictColumns, ...setColumns];
 
-  const skipUpdateColumns = [...skipInsertColumns, ...onConflictColumns];
-
   try {
-    const querySql = createUpsertQuerySql(
-      data,
-      table,
-      columns,
-      onConflictColumns,
-      skipUpdateColumns
-    );
+    const querySql = createUpsertQueryCustomSetValuesLogicSql(data, table, columns, onConflictColumns, setColumnsStr);
 
     return db.any(querySql);
   } catch (e) {
