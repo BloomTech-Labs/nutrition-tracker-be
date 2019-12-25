1042ab1d6a4f50b2333ec8b5790938ab0e4117f1
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-25 00:28:36 -0500

    removes undefined export

--- routes/settings/usersDB.js
+++ routes/settings/usersDB.js
@@ -10,8 +10,7 @@ module.exports = {
   findWeightGoalById,
   addWeightGoal,
   findActivityLevelById,
-  addActivityLevel,
-  getCaloricBudget
+  addActivityLevel
 };
 
 /********************************************************
