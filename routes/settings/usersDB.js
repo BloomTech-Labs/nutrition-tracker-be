b99730d16b49491f0869b5c47f4aec57bb6e3a02
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-24 23:20:10 -0500

    removes unused db function

--- routes/settings/usersDB.js
+++ routes/settings/usersDB.js
@@ -11,8 +11,7 @@ module.exports = {
   addWeightGoal,
   findActivityLevelById,
   addActivityLevel,
-  getCaloricBudget,
-  getDailyLog
+  getCaloricBudget
 };
 
 /********************************************************
@@ -118,28 +117,4 @@ function getCaloricBudget(user_id) {
     .select("caloric_budget", "fat_ratio", "protein_ratio", "carb_ratio")
     .where({ user_id })
     .first();
-}
-
-function getDailyLog(user_id, from, to) {
-  return db("food_log as fl")
-    .join("foods as f", {
-      "fl.food_id": "f.id"
-    })
-    .select(
-      "fl.food_id as foodID",
-      "fl.fatsecret_food_id as fatSecretFoodID",
-      "fl.time_consumed_at as timeConsumedAt",
-      "fl.time_zone_name as timeZoneName",
-      "fl.time_zone_abbr as timeZoneAbbr",
-      "fl.quantity",
-      "f.food_name as foodName",
-      "f.serving_desc as servingDescription",
-      "f.calories_kcal as caloriesKcal",
-      "f.fat_g as fatGrams",
-      "f.carbs_g as carbsGrams",
-      "f.protein_g as proteinGrams"
-    )
-    .where("fl.user_id", "=", user_id)
-    .whereBetween("fl.time_consumed_at", [from, to])
-    .orderBy("fl.time_consumed_at");
 }
