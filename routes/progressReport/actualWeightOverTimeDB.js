38e2829e3d6b2a14d0e6b5eb6fc6acf3ebfe1ed5
Will Ediger <will.ediger@gmail.com>
2019-12-25 20:38:49 -0600

    renamed "weight over time" to "actual weight over time" for precision

--- routes/progressReport/actualWeightOverTimeDB.js
+++ routes/progressReport/actualWeightOverTimeDB.js
@@ -1,9 +1,9 @@
 const { db: pgPromiseDb } = require("../../data/pg-promise.js");
 
-const weightOverTime = async (user_id, time_zone, goal_start_date, goal_end_date) => {
+const actualWeightOverTime = async (user_id, time_zone, goal_start_date, goal_end_date) => {
   //calculates the actual_weight_kg in force at a given observation_date within the date range from "goal start date" to "goal end date"
   const queryString = require("./sql/actual_weights_over_time")(user_id, time_zone, goal_start_date, goal_end_date);
   return await pgPromiseDb.any(queryString);
 };
 
-module.exports = weightOverTime;
+module.exports = actualWeightOverTime;
--- routes/progressReport/devRun.js
+++ routes/progressReport/devRun.js
@@ -3,14 +3,14 @@ const time_zone = `CST`;
 const goal_start_date = `2019-12-15`;
 const goal_end_date = `2020-01-31`;
 
-const weightOverTime = require("./weightOverTimeDB");
+const actualWeightOverTime = require("./actualWeightOverTimeDB");
 const goalWeightOverTime = require("./goalWeightOverTimeDB");
 
 async function main() {
-  //executes weightOverTime function with some hardcoded example values which creates SQL and runs the query in pg-promise
-  console.log(await weightOverTime(user_id, time_zone, goal_start_date, goal_end_date));
+  //executes actualWeightOverTime function with some hardcoded example values which creates SQL and runs the query in pg-promise
+  console.log(await actualWeightOverTime(user_id, time_zone, goal_start_date, goal_end_date));
 
-  //executes weightOverTime function with some hardcoded example values which creates SQL and runs the query in pg-promise
+  //executes goalWeightOverTime function with some hardcoded example values which creates SQL and runs the query in pg-promise
   console.log(await goalWeightOverTime(user_id, time_zone, goal_start_date, goal_end_date));
 }
 
