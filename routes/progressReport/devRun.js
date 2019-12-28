b95af41269d977cf5292205a60c71d7b55fc1b96
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-28 18:52:48 -0500

    reformats dev function

--- routes/progressReport/devRun.js
+++ routes/progressReport/devRun.js
@@ -9,13 +9,41 @@ const weightOverTime = require("./weightOverTimeDB");
 
 async function main() {
   //executes actualWeightOverTime function with some hardcoded example values which creates SQL and runs the query in pg-promise
-  // console.log(await actualWeightOverTime(user_id, time_zone, start_date, end_date));
 
-  //executes goalWeightOverTime function with some hardcoded example values which creates SQL and runs the query in pg-promise
-  // console.log(await goalWeightOverTime(user_id, time_zone, start_date, end_date));
+  const actual_weight_over_time = await actualWeightOverTime(
+    user_id,
+    time_zone,
+    start_date,
+    end_date
+  );
+
+  const goal_weight_over_time = await goalWeightOverTime(
+    user_id,
+    time_zone,
+    start_date,
+    end_date
+  );
+
+  const weight_over_time = await weightOverTime(
+    user_id,
+    time_zone,
+    start_date,
+    end_date
+  );
+
+  // console.log("actualWeightOverTime:\n", actual_weight_over_time);
+
+  // //executes goalWeightOverTime function with some hardcoded example values which creates SQL and runs the query in pg-promise
+  // console.log("goalWeightOverTime:\n", goal_weight_over_time);
 
   //executes weightOverTime function with some hardcoded example values which creates SQL and runs the query in pg-promise
-  console.log(await weightOverTime(user_id, time_zone, start_date, end_date));
+  console.log("weightOverTime:\n", weight_over_time);
+
+  // console.log("actual_weight_over_time:", actual_weight_over_time.length);
+  // console.log("goal_weight_over_time:", goal_weight_over_time.length);
+  console.log("weight_over_time:", weight_over_time.length);
 }
 
-main();
+// main();
+
+module.exports = main;
