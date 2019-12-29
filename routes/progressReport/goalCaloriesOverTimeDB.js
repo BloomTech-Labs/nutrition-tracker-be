9488b93c366970bc665a0f23be4832373275ed62
Will Ediger <will.ediger@gmail.com>
2019-12-29 17:17:16 -0600

    actual calories over time db functionality, "devRun" file, and placeholders for "goal calories over time" and "calories over time" (both actual and goal)

---
+++ routes/progressReport/goalCaloriesOverTimeDB.js
@@ -0,0 +1,10 @@
+const { db: pgPromiseDb } = require("../../data/pg-promise.js");
+
+const goalCaloriesOverTimeDB = async (user_id, start_date, end_date) => {
+  //calculates the goal calories consumed on a given observation_date within the date range from "goal start date" to "goal end date"
+  const queryString = require("./sql/goal_calories_over_time")(user_id, start_date, end_date);
+  console.log(queryString);
+  return await pgPromiseDb.any(queryString);
+};
+
+module.exports = goalCaloriesOverTimeDB;
