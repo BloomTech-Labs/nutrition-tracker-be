9f884a6695ffff05eb7965662a984f65ff96afea
Will Ediger <will.ediger@gmail.com>
2019-12-29 18:31:50 -0600

    changed reference in "target weight over time" to "actual weights" table to "goal weights" table for consistency/clarity

--- routes/progressReport/sql/weights_over_time.js
+++ routes/progressReport/sql/weights_over_time.js
@@ -11,8 +11,8 @@ const weights_over_time = (user_id, time_zone, start_date, end_date) => {
     aw_ot.observation_date,
     aw_ot.actual_weight_kg,
     round(
-      gw_ot.goal_weight_kg - gr_ot.goal_daily_weight_kg_change_rate * 
-        (gw_ot.goal_end_date::date - aw_ot.observation_date::date) --number of days remaining to goal end date
+        gw_ot.goal_weight_kg - gr_ot.goal_daily_weight_kg_change_rate * 
+        (gw_ot.goal_end_date::date - gw_ot.observation_date::date) --number of days remaining to goal end date
       , 2
     ) as target_goal_weight_kg
   from (${actual_weights_over_time}) as aw_ot
