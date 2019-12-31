f28ec1404e829b11f04aa7bffad5eac00b22b131
Dave Vazquez <dmvaz.li@gmail.com>
2019-12-30 22:37:13 -0500

    formats

--- routes/progressReport/weightOverTimeDB.js
+++ routes/progressReport/weightOverTimeDB.js
@@ -2,8 +2,13 @@ const { db: pgPromiseDb } = require("../../data/pg-promise.js");
 
 const weightOverTimeDB = async (user_id, time_zone, start_date, end_date) => {
   //calculates the actual_weight_kg in force at a given observation_date within the date range from "goal start date" to "goal end date"
-  const queryString = require("./sql/weights_over_time")(user_id, time_zone, start_date, end_date);
-  console.log(queryString);
+  const queryString = require("./sql/weights_over_time")(
+    user_id,
+    time_zone,
+    start_date,
+    end_date
+  );
+  // console.log(queryString);
   return await pgPromiseDb.any(queryString);
 };
 
