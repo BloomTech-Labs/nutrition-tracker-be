f4fd30d4e535efd25d20a577e230c21e0293442d
Will Ediger <will.ediger@gmail.com>
2020-01-04 23:37:12 -0600

    limiting goal weights (and target weights) over time by current/last goal_end_date

--- routes/progressReport/sql/goal_weights_over_time.js
+++ routes/progressReport/sql/goal_weights_over_time.js
@@ -7,6 +7,12 @@ const goal_weights_over_time = (user_id, time_zone, start_date, end_date) => {
     start_date,
     end_date
   );
+  const last_observation_date = require("./actual_weights_last_observation_date")(
+    user_id,
+    time_zone,
+    start_date,
+    end_date
+  );
   return `
     select 
       d_a_d.user_id,
@@ -19,7 +25,8 @@ const goal_weights_over_time = (user_id, time_zone, start_date, end_date) => {
       d_a_d.user_id = ubd.user_id and
       d_a_d.applicable_date = ubd.applicable_date
     where
-      ubd.goal_weight_kg is not null
+      ubd.goal_weight_kg is not null AND
+      d_a_d.observation_date <= ${last_observation_date}
     order by
       d_a_d.observation_date
   `;
