f45e96e8c4f1e2dab0c85de29ebd3f2cd1d66bdb
JimmyMcBride <mcbride967@gmail.com>
2020-01-06 23:46:34 -0600

    Merge branch 'macros-over-time-db' of github.com:Lambda-School-Labs/nutrition-tracker-be into progress-reports-macros

--- routes/progressReport/sql/goal_carbs_macros_over_time.js
+++ routes/progressReport/sql/goal_carbs_macros_over_time.js
@@ -16,7 +16,7 @@ const goal_carbs_macros_goal_over_time = (
     select 
       d_a_d.user_id,
       d_a_d.observation_date,
-      ROUND(ubd.carb_ratio::decimal * ubd.caloric_budget / 4, 2) as goal_carbs_macros
+      ROUND(ubd.carb_ratio::decimal * 100) as goal_carbs_macros
     from ${daily_applicable_dates} as d_a_d
     inner join user_budget_data as ubd
     on 
