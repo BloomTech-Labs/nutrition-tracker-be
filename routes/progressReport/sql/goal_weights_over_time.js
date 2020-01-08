3cc936222e2f3de47c8dca7210e0a10e408e6f00
Will Ediger <will.ediger@gmail.com>
2020-01-07 23:26:57 -0600

    removed where condition of `last_observation_date`, as that attribute of the results is already being accounted for

--- routes/progressReport/sql/goal_weights_over_time.js
+++ routes/progressReport/sql/goal_weights_over_time.js
@@ -7,12 +7,7 @@ const goal_weights_over_time = (user_id, time_zone, start_date, end_date) => {
     start_date,
     end_date
   );
-  const last_observation_date = require("./goal_weights_last_observation_date")(
-    user_id,
-    time_zone,
-    start_date,
-    end_date
-  );
+
   return `
     select 
       d_a_d.user_id,
@@ -25,8 +20,7 @@ const goal_weights_over_time = (user_id, time_zone, start_date, end_date) => {
       d_a_d.user_id = ubd.user_id and
       d_a_d.applicable_date = ubd.applicable_date
     where
-      ubd.goal_weight_kg is not null AND
-      d_a_d.observation_date <= ${last_observation_date}
+      ubd.goal_weight_kg is not null 
     order by
       d_a_d.observation_date
   `;
